f40f243c9a2c896e5c56f3211654f22f86616271
czomii@gmail.com <czomii@gmail.com>
2023-04-15 17:06:46 +0200

    springFramework

--- src/test/java/eu/spring_project_001/demo/service/car/CarControllerGetMethodITest.java
+++ src/test/java/eu/spring_project_001/demo/service/car/CarControllerGetMethodITest.java
@@ -1,5 +1,6 @@
 package eu.spring_project_001.demo.service.car;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,45 +8,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Sql(scripts = "classpath:schema.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "classpath:drop-schema.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @AutoConfigureMockMvc
 @SpringBootTest
-class CarControllerITTest {
+class CarControllerGetMethodITest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Transactional
+
     @Test
-    void addCar() throws Exception
+    void getCarFromDataBase() throws Exception
     {
         // given
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json;charset=utf-8");
         headers.add("Accept", "application/json;charset=utf-8");
-        String requestYourBody = prepareRequest();
 
         // when, // then
-        mockMvc.perform(post("/cars/add").content(requestYourBody).headers(headers)).andDo(print()).andExpect(status().isCreated());
-    }
-
-    private String prepareRequest()
-    {
-        return """
-                {
-                    "model" : "Veyron Grand Sport EB",
+        mockMvc.perform(get("/cars").param("parameter", "BUGATTI").headers(headers)).andDo(print()).andExpect(status().isOk()).andExpect(content().json("""
+                    [{
+                    "model" :"Veyron Grand Sport EB",
                     "makeOfCar" : "BUGATTI",
                     "typeOfEngine" : "PETROL",
-                    "ownerId" : 110
-                }
-                """;
+                    "id" : 210
+                    }]
+                    """));
     }
-
 }
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,2 +1,5 @@
 INSERT INTO owner (id, first_name , last_name, age, identity, street_name, postal_code, city, available_amount_of_money)
 VALUES (110, 'Kamil', 'Chojecki', 21, '012202893', 'Lipowa 12', 11010, 'Barczewo', 2.8);
+
+INSERT INTO car (id, type_of_engine , model , make_of_car, owner_id)
+VALUES (210, 'PETROL',  'Veyron Grand Sport EB',  'BUGATTI', 110);
